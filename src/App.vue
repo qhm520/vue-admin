3f3301b232f67ce3b50351d3b1969442dd4739f0
QIAN <794308528@qq.com>
2020-03-13 17:21:32 +0800

    vue-admin

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+       <router-view></router-view>
+      <footer-guide v-show="$route.meta.showFooter"/>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  export default {
+    name: 'App',
+    components: {
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
